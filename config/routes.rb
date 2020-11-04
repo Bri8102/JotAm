f55dbed46439056971f6c1a17283d5a987cfac0a
Bri8102 <damioniru16@gmail.com>
2020-11-04 00:28:25 -0500

    nested routes for tasks in list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :lists
+
+  resources :lists do
+    resources :tasks do
+     member do
+      patch :done
+     end
+   end
+ end
+
   root "lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
