a7dbfe9aec8486633fc8d79d57eb9ef404026f4a
Bri8102 <damioniru16@gmail.com>
2020-11-16 11:23:06 -0500

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+    # def index
+    #     @users = User.all
+    # end
+
+    def show
+        # redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :username, :email, :password, :password_confirmation)
+    end
+end
