6fbdc5ece9df929d33a8647d93c22e3c33190c66
Bri8102 <damioniru16@gmail.com>
2020-11-16 11:19:55 -0500

    created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+ 
+    helper_method :redirect_if_logged_in, :logged_in?, :current_user, :redirect_if_not_logged_in
+
+    def logged_in?
+        #checks if session is set to user-ID i.e if user is logged in returns true if so 
+        !!session[:user_id]
+    end
+
+    def current_user
+        @user ||= User.find_by(id: session[:user_id])
+    end 
+   
+    
+    def redirect_if_not_logged_in
+        if !logged_in?
+          flash[:errors] = "Log in to View."
+          redirect_to "/login"
+        end 
+    end 
+  
+    def redirect_if_logged_in 
+        if logged_in?
+          redirect_to "/"
+        end
+    end 
 end
