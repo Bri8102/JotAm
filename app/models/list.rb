dc06906205337e7ddf4736e276b3c58890691c9a
Bri8102 <damioniru16@gmail.com>
2020-11-24 01:22:29 -0500

    added validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
     belongs_to :user
     has_many :tasks, :dependent => :destroy
- 
- 
-    # validates_presence_of :user
+    accepts_nested_attributes_for :tasks
+
+    validates :name, presence: true
+    validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 end
